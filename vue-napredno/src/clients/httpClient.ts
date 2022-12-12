dca6a4134ca5a7c61db64981f9d6ebe0f8ce9398
Domagoj Mahnet <domagoj.mahnet@king-ict.cloud>
2022-12-12 11:12:51 +0100

    Request interceptor

--- vue-napredno/src/clients/httpClient.ts
+++ vue-napredno/src/clients/httpClient.ts
@@ -1,10 +1,29 @@
+import { UserStore } from "./../store/user-store";
 import axios, { AxiosInstance } from "axios";
 
 const httpClient: AxiosInstance = axios.create({
   baseURL: "https://localhost:44351/api",
   headers: {
     "Content-type": "application/json"
-  }
+  },
+  withCredentials: true
 });
+
+httpClient.interceptors.request.use(
+  (config) => {
+    const userStore = UserStore();
+    let token = userStore.token;
+    if (token !== "") {
+      config.headers = {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`
+      };
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default httpClient;
